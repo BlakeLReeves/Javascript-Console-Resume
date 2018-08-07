7a90b04d41f521c48688b5b5eb969b042b64e8cc
blakereeves19 <blake.reeves19@gmail.com>
2018-08-07 17:41:45 -0500

    Updated repo

---
+++ app.js
@@ -0,0 +1,20 @@
+function displayPosition(companyName, jobTitle, description) {
+    return jobTitle + companyName + description;
+}
+function displaySkill(skillName, booleanValue) {
+    return 
+}
+
+console.log('Name:', 'Blake Reeves'.toUpperCase(), '\nCareer: Pest Control Technician', '\nDescription: I kill bugs');
+
+console.log('My Interests:', '\n* Video games', '\n* Reading', '\n* Coding', '\n* Music');
+
+let a = displayPosition("O'Charley's - ", "* Guest Assistant at ", "Greeted and seated guests as they came into the restaurant, and helped clean off tables.")
+console.log('My Previous Experience:')
+console.log(a);
+
+let b = displayPosition("Brooks Shaw's Old Country Store - ", "* Barista at ", "Took orders from guests, and served them coffee.")
+console.log(b);
+
+let c = displayPosition("Blankenship Farms - ", "* Farm hand at ", "Tended to the cattle farm, and helped with the various tasks around the farm.")
+console.log(c);
